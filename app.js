9de49c2e5de5e88ce67b4af2cdbd0474881f466b
AthallaITS <5027231018@student.its.ac.id>
2023-10-23 13:02:55 +0700

    init

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
-// app.js
 const express = require('express');
-const bodyParser = require('body-parser');
-const libraryData = require('./data'); // Impor data dari data.js
+const libraryData = require('./data');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Endpoint untuk menambahkan buku
-app.get('/hello', (req, res) => {
-    res.json('Hello World');
- })
+// Endpoint untuk menambahkan buku baru ke dalam database
+app.post('/addBook', (req, res) => {
+  const { body } = req;
+  libraryData.push(body); // Menambahkan buku baru ke dalam libraryData
+  return res.json(libraryData); // Mengembalikan data buku yang diperbarui sebagai respons
+});
 
+// Endpoint untuk mendapatkan daftar semua buku dalam database
+app.get('/getBooks', (req, res) => {
+  return res.json(libraryData); // Mengembalikan seluruh data buku sebagai respons
+});
 
-const port = process.env.PORT || 3000;
+// Endpoint untuk mengedit buku berdasarkan indeks
+app.put('/editBook/:index', (req, res) => {
+  const { body } = req;
+  const index = parseInt(req.params.index);
+
+  if (index >= 0 && index < libraryData.length) {
+    libraryData[index] = body; // Mengganti buku yang ada pada indeks tertentu dengan data baru
+    return res.json(libraryData); // Mengembalikan data buku yang diperbarui sebagai respons
+  } else {
+    return res.status(404).json({ error: 'Buku tidak ditemukan' });
+  }
+});
+
+// Endpoint untuk menghapus buku berdasarkan indeks
+app.delete('/deleteBook/:index', (req, res) => {
+  const index = parseInt(req.params.index);
+
+  if (index >= 0 && index < libraryData.length) {
+    libraryData.splice(index, 1); // Menghapus buku pada indeks tertentu dari libraryData
+    return res.json(libraryData); // Mengembalikan data buku yang diperbarui sebagai respons
+  } else {
+    return res.status(404).json({ error: 'Buku tidak ditemukan' });
+  }
+});
+
+const port = 4000;
 app.listen(port, () => {
-    console.log(`Server berjalan di port ${port}`);
+  console.log(`server running on ${port}`);
 });
