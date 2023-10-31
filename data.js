6fb3bdfc08d846b0e8d83ea8a53abf7efdb44d2d
heradph <devarizaph@gmail.com>
2023-10-31 23:21:00 +0700

    data rest

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 // data.js
 const libraryData = [
-  { title: 'Sponsbob', author: 'Clive Staples Lewis', published: '16 Oktober 1950' },
-  { title: 'Sherlock Holmes', author: 'Arthur Conan Doyle', published: '1 Desember 1887' },
-  { title: 'The Lord of the Rings', author: 'J. R. R. Tolkien', published: '29 Juli 1954' },
-  // Tambahkan data buku lainnya sesuai kebutuhan
+  { title: 'The Man Who Cant Be Moved', writer: 'The Script', published: '17 November 2017' },
+  { title: 'Blue Jeans', writer: 'Gangga', published: '29 Maret 2018' },
+  { title: 'Before You Go', writer: 'Lewis Capaldi', published: '15 April 2019' },
+  // Tambahkan data lagu lainnya sesuai kebutuhan
 ];
 
 module.exports = libraryData;
