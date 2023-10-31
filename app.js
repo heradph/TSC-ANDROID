a67267fddfd0254508e14f951d0397cd13d619ac
heradph <devarizaph@gmail.com>
2023-10-31 23:43:29 +0700

    koe

--- app.js
+++ app.js
@@ -5,38 +5,38 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Endpoint untuk menambahkan buku baru ke dalam database
+// Endpoint menambahkan lagu baru ke database
 app.post('/addBook', (req, res) => {
   const { body } = req;
-  libraryData.push(body); // Menambahkan buku baru ke dalam libraryData
-  return res.json(libraryData); // Mengembalikan data buku yang diperbarui sebagai respons
+  libraryData.push(body); // Menambahkan lagu baru ke dalam libraryData
+  return res.json(libraryData); // Mengembalikan data lagu yang diperbarui sebagai respons
 });
 
-// Endpoint untuk mendapatkan daftar semua buku dalam database
+// Endpoint untuk mendapatkan daftar semua lagu dalam database
 app.get('/getBooks', (req, res) => {
-  return res.json(libraryData); // Mengembalikan seluruh data buku sebagai respons
+  return res.json(libraryData); // Mengembalikan seluruh data lagu sebagai respons
 });
 
-// Endpoint untuk mengedit buku berdasarkan indeks
+// Endpoint untuk mengedit lagu berdasarkan indeks
 app.put('/editBook/:index', (req, res) => {
   const { body } = req;
   const index = parseInt(req.params.index);
 
   if (index >= 0 && index < libraryData.length) {
-    libraryData[index] = body; // Mengganti buku yang ada pada indeks tertentu dengan data baru
-    return res.json(libraryData); // Mengembalikan data buku yang diperbarui sebagai respons
+    libraryData[index] = body; // Mengganti lagu yang ada pada indeks tertentu dengan data baru
+    return res.json(libraryData); // Mengembalikan data lagu yang diperbarui sebagai respons
   } else {
     return res.status(404).json({ error: 'Buku tidak ditemukan' });
   }
 });
 
-// Endpoint untuk menghapus buku berdasarkan indeks
+// Endpoint untuk menghapus lagu berdasarkan indeks
 app.delete('/deleteBook/:index', (req, res) => {
   const index = parseInt(req.params.index);
 
   if (index >= 0 && index < libraryData.length) {
-    libraryData.splice(index, 1); // Menghapus buku pada indeks tertentu dari libraryData
-    return res.json(libraryData); // Mengembalikan data buku yang diperbarui sebagai respons
+    libraryData.splice(index, 1); // Menghapus lagu pada indeks tertentu dari libraryData
+    return res.json(libraryData); // Mengembalikan data lagu yang diperbarui sebagai respons
   } else {
     return res.status(404).json({ error: 'Buku tidak ditemukan' });
   }
