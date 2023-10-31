ada12b7320bb68aae84aee0cf899b621083805c9
heradph <devarizaph@gmail.com>
2023-10-31 22:00:56 +0700

    update title

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // data.js
 const libraryData = [
-  { title: 'The Chronicles of Narnia', author: 'Clive Staples Lewis', published: '16 Oktober 1950' },
+  { title: 'Sponsbob', author: 'Clive Staples Lewis', published: '16 Oktober 1950' },
   { title: 'Sherlock Holmes', author: 'Arthur Conan Doyle', published: '1 Desember 1887' },
   { title: 'The Lord of the Rings', author: 'J. R. R. Tolkien', published: '29 Juli 1954' },
   // Tambahkan data buku lainnya sesuai kebutuhan
